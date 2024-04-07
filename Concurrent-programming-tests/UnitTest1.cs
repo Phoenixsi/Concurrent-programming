f7e308a3fa4a9e2de7dfdde8b58629f0d7a5a512
Phoenixsi <niepotrzebnysyf123@gmail.com>
2024-04-08 00:03:04 +0200

    Zmiany na WPF

--- Concurrent-programming-tests/UnitTest1.cs
+++ Concurrent-programming-tests/UnitTest1.cs
@@ -1,24 +1,11 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.IO;
-using System;
-
-namespace HelloWorldTests
+namespace Concurrent_programming_tests
 {
     [TestClass]
     public class UnitTest1
     {
-        private const string Expected = "Hello World!";
         [TestMethod]
         public void TestMethod1()
         {
-            using (var sw = new StringWriter())
-            {
-                Console.SetOut(sw);
-                HelloWorld.Program.Main();
-
-                var result = sw.ToString().Trim();
-                Assert.AreEqual(Expected, result);
-            }
         }
     }
 }
