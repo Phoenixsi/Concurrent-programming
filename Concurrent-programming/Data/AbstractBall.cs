f55d8c832c5569c771cab324b5c6ae86daf4a142
Phoenixsi <niepotrzebnysyf123@gmail.com>
2024-04-10 18:08:46 +0200

    Dodana klasa abstrakcyjna AbstractBall do Werstyw Data

---
+++ Concurrent-programming/Data/AbstractBall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public abstract class AbstractBall : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public abstract int BallID { get; }
+        public abstract Vector<double> BallPosition { get; set; }
+        public abstract Vector<double> BallVelocity { get; set; }
+        public abstract double BallSize { get; set; }
+        public abstract double BallMass { get; set; }
+
+    }
+}
