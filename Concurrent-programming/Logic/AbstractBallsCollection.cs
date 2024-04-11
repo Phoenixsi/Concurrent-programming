816c104c82eca5bbba5ee6135bab68e7708a3c8e
Phoenixsi <niepotrzebnysyf123@gmail.com>
2024-04-12 00:24:37 +0200

    Lekkie poprawki

--- Concurrent-programming/Logic/AbstractBallsCollection.cs
+++ Concurrent-programming/Logic/AbstractBallsCollection.cs
@@ -10,12 +10,16 @@ namespace Concurrent_programming.Logic
     public abstract class AbstractBallsColletors : INotifyCollectionChanged, IDisposable
     {
         public abstract int Count { get; }
-
         public abstract void AddBall();
         public abstract void RemoveBall();
         public abstract void ClearBall();
         public abstract void Dispose();
-            
+        public abstract void StartTimer();
+        public abstract void StopTimer();
+        public abstract void InitBalls(int ballsNumber);
+        public abstract void SetNewArea(double width, double height);
+        public abstract void SetNewSize(double size);
+
 
         public event NotifyCollectionChangedEventHandler? CollectionChanged;
 
