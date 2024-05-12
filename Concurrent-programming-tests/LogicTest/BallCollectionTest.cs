86ce62e1d5a8c5613f42dfdba5747fe20ed3b048
Kasia <240761@edu.p.lodz.pl>
2024-05-12 22:59:29 +0200

    Aktualizacja testów

--- Concurrent-programming-tests/LogicTest/BallCollectionTest.cs
+++ Concurrent-programming-tests/LogicTest/BallCollectionTest.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -66,22 +67,47 @@ namespace LogicTest
             Assert.IsTrue(ballsCollection.CountedBalls.Equals(0));
         }
 
-/*        [TestMethod]
-        public async Task PostionAfterFrameUpdate()
+        public void PositionAndVelocityAfterFrameUpdate()
         {
-            AbstractBallsCollection ballsCollection = new BallsCollection(500, 800);
-            ballsCollection.AddBall();
+            // Setup a new BallsCollection with specific canvas dimensions
+            BallsCollection ballsCollection = new BallsCollection(500, 800);
+            ballsCollection.AddBall();  // Adding a single ball
 
-            AbstractBall ballBefor = ballsCollection.Balls[0];
+            // Capture the initial state of the ball
+            Ball initialBall = (Ball)ballsCollection.Balls[0];
+            Vector2 initialPosition = initialBall.BallPosition;
+            Vector2 initialVelocity = initialBall.BallVelocity;
 
-            ballsCollection.StartTimer();
-            await Task.Delay(20);
-            ballsCollection.StopTimer();
+            // Run the update frame method which simulates one frame of movement
+            ballsCollection.UpdateFrame();
 
-            Assert.AreNotEqual(ballBefor.BallPosition, ballsCollection.Balls[0].BallPosition);
-            Assert.AreNotEqual(ballBefor.BallVelocity, ballsCollection.Balls[0].BallVelocity);
+            // Capture the new state of the ball after the frame update
+            Ball updatedBall = (Ball)ballsCollection.Balls[0];
+            Vector2 updatedPosition = updatedBall.BallPosition;
+            Vector2 updatedVelocity = updatedBall.BallVelocity;
+
+            // Check that the position has changed according to the velocity
+            Assert.AreEqual(initialPosition + initialVelocity, updatedPosition);
+
+            // Since no collisions are expected with a single ball, velocity should remain unchanged
+            Assert.AreEqual(initialVelocity, updatedVelocity);
         }
-*/
+        /*        [TestMethod]
+                public async Task PostionAfterFrameUpdate()
+                {
+                    AbstractBallsCollection ballsCollection = new BallsCollection(500, 800);
+                    ballsCollection.AddBall();
+
+                    AbstractBall ballBefor = ballsCollection.Balls[0];
+
+                    ballsCollection.StartTimer();
+                    await Task.Delay(20);
+                    ballsCollection.StopTimer();
+
+                    Assert.AreNotEqual(ballBefor.BallPosition, ballsCollection.Balls[0].BallPosition);
+                    Assert.AreNotEqual(ballBefor.BallVelocity, ballsCollection.Balls[0].BallVelocity);
+                }
+        */
 
         [TestMethod]
         public void CollectionChange()
